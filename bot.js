20b811c8a356e3d3ea9bedd3c7eb9598f38429f0
HeyImAbOoD <42396013+HeyImAbOoD@users.noreply.github.com>
2018-08-25 02:31:50 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -605,7 +605,6 @@ client.on('voiceStateUpdate', (old, now) => {
 var prefix = "$";
 client.on('message', message => {
      if(message.author.bot) return;
-
     if (!message.content.startsWith(prefix)) return;
     let command = message.content.split(" ")[0];
     command = command.slice(prefix.length);
@@ -632,7 +631,6 @@ client.on('message', message => {
 var prefix = "$";
 client.on('message', message => {
      if(message.author.bot) return;
-
     if (!message.content.startsWith(prefix)) return;
     let command = message.content.split(" ")[0];
     command = command.slice(prefix.length);
